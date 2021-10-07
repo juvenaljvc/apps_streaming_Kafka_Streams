755db2243a9130de4fcd8b2450078ca759d5f4b6
Juvenal JVC <jvc35@yahoo.fr>
2021-10-07 17:12:22 +0200

    Utilisation d'un custom SerDes dans la topologie Kafka Streams- wrapper et mécanique

--- src/main/scala/CustomSerdesUsage.scala
+++ src/main/scala/CustomSerdesUsage.scala
@@ -1,11 +1,13 @@
 import org.apache.kafka.streams.scala._
-
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.{StreamsConfig, Topology}
 import org.apache.kafka.streams.scala.kstream._
 import HelloWorld_KafkaStreams._
 import schemas.{Facture, OrderLine}
-import serdes.BytesSerDes
+import serdes.{BytesDeserializer, BytesSerDes, BytesSerializer}
+
+// 2ème méthode pour l'utilisation des custom serdes
+import org.apache.kafka.common.serialization.{Serde, Serdes}
 
 object CustomSerdesUsage extends App {
 
@@ -13,8 +15,14 @@ object CustomSerdesUsage extends App {
   import org.apache.kafka.streams.scala.Serdes.String
   import org.apache.kafka.streams.scala.Serdes.Integer
 
+  // 1ère méthode pour l'utilisation des custom serdes
   implicit val bytesSerdes = new BytesSerDes[Facture]
   implicit val bytesOrdersSerdes = new BytesSerDes[OrderLine]
+
+  // 2ème méthode pour l'utilisation des custom serdes
+  implicit val factureSerdes : Serde[Facture] = Serdes.serdeFrom[Facture](new BytesSerializer[Facture], new BytesDeserializer[Facture])
+  implicit val consumed : Consumed[String, Facture] = Consumed.`with`(Serdes.String(), factureSerdes)
+  implicit val produced : Produced[String, Facture] = Produced.`with`(Serdes.String(), factureSerdes)
 
   val str : StreamsBuilder = new StreamsBuilder
   val kstr : KStream[String, Facture]  = str.stream[String, Facture]("streams_app")
