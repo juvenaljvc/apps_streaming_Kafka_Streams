5402c881a8bc361fb9d08c45a1ca367828a6df01
Juvenal JVC <jvc35@yahoo.fr>
2021-11-13 03:03:24 +0100

    fenêtrage, sessions, gestion des retardataires, clôture des fenêtres

--- src/main/scala/processors/statefull/WindowingProcess.scala
+++ src/main/scala/processors/statefull/WindowingProcess.scala
@@ -7,8 +7,9 @@ import org.apache.kafka.streams.scala.kstream._
 import schemas.{Facture, OrderLine}
 import serdes.{JSONDeserializer, JSONSerializer}
 import org.apache.kafka.common.serialization.{Serde, Serdes}
-import org.apache.kafka.streams.kstream.Printed
+import org.apache.kafka.streams.kstream.{Printed, SessionWindows, TimeWindows, Windowed}
 
+import java.time.{Duration, ZoneOffset}
 import java.util.Properties
 
 
@@ -24,7 +25,7 @@ object WindowingProcess extends App {
 
 
   val props : Properties = new Properties()
-  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "windowingProcess1")
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "windowingProcess-interm2")
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
   props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, "org.apache.kafka.streams.processor.WallclockTimestampExtractor")
   props.put("message.timestamp.type", "LogAppendTime")
@@ -32,25 +33,42 @@ object WindowingProcess extends App {
   val str : StreamsBuilder = new StreamsBuilder
   val kstrFacture : KStream[String, Facture]  = str.stream[String, Facture]("factureJson")(consumed)
 
-  val kCA = kstrFacture
-    .map((k, f) => ("1", f.total))
+  // fenêtres fixes
+  val kCA : KTable[Windowed[String], Long]= kstrFacture
+    .map((k, f) => (k, f.total))
     .groupBy((k, t) => k)(Grouped.`with`(String, Double))
-    .aggregate[Double](0D)((key, newValue, aggValue) => aggValue + newValue)
+    .windowedBy(TimeWindows.of(Duration.ofSeconds(15)).grace(Duration.ofSeconds(5)))  // rajout d'une période de grace pour clôturer la fenêtre
+    .count()
 
-  kCA.toStream.print(Printed.toSysOut().withLabel("Chiffre d'affaire global"))
+  // fenêtres glissantes
+  val kCA1 : KTable[Windowed[String], Long]= kstrFacture
+    .map((k, f) => (k, f.total))
+    .groupBy((k, t) => k)(Grouped.`with`(String, Double))
+    .windowedBy(TimeWindows.of(Duration.ofSeconds(15)).advanceBy(Duration.ofSeconds(3)))
+    .count()
 
-  // méthode 2
-  val kCA2 = kstrFacture
+  // Session
+  val kCA3 : KTable[Windowed[String], Long]= kstrFacture
+    .map((k, f) => (k, f.total))
+    .groupBy((k, t) => k)(Grouped.`with`(String, Double))
+    .windowedBy(SessionWindows.`with`(Duration.ofMinutes(5)))
+    .count()
+
+  kCA.toStream.foreach(
+    (key, value) => println(
+      s"clé de la fenêtre : ${key}, " +
+      s"clé du message : ${key.key()}, " +
+      s"debut : ${key.window().startTime().atOffset(ZoneOffset.UTC)}, " +
+      s"fin : ${key.window().endTime().atOffset(ZoneOffset.UTC)}, " +
+      s"valeur : ${value}"
+    )
+  )
+
+  // calcul du chiffre d'affaire moyen réalisé toutes les 15 secondes
+  val kCA4 = kstrFacture
     .map((k, f) => ("1", f))
     .groupBy((k, t) => k)
-    .aggregate[Double](0)((k,newFacture, aggFacture) => newFacture.total + aggFacture)
-
-  kCA2.toStream.print(Printed.toSysOut().withLabel("Chiffre d'affaire global - M2"))
-
-  // calcul du chiffre d'affaire moyen
-  val kCA3 = kstrFacture
-    .map((k, f) => ("1", f))
-    .groupBy((k, t) => k)
+    .windowedBy(TimeWindows.of(Duration.ofSeconds(15)))
     .aggregate[Facture](Facture("", "", 0, 0D, OrderLine("", "", "", "", 0D,0D, 0)))(
       (key, newFacture, aggFacture) =>
         Facture(newFacture.factureid, "",
@@ -58,7 +76,6 @@ object WindowingProcess extends App {
           newFacture.total + aggFacture.total, OrderLine("", "", "", "", 0D,0D, 0))
     ).mapValues(f => (f.qantite, f.total, f.total/f.qantite))
 
-  kCA3.toStream.print(Printed.toSysOut().withLabel("Panier moyen du consommateur - M3"))
 
   val topologie : Topology = str.build()
   val kkStream : KafkaStreams = new KafkaStreams(topologie, props)
