ada46c3733536cc80e153d407292f6b55ba11ede
Juvenal JVC <jvc35@yahoo.fr>
2021-12-04 06:56:54 +0100

    Jointure KStream-à-KStream

--- src/main/scala/processors/statefull/KstreamKstreamJoin.scala
+++ src/main/scala/processors/statefull/KstreamKstreamJoin.scala
@@ -1,13 +1,13 @@
 package processors.statefull
 
 import org.apache.kafka.streams.scala._
-import org.apache.kafka.streams.KafkaStreams
-import org.apache.kafka.streams.{StreamsConfig, Topology}
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, Topology, scala}
 import org.apache.kafka.streams.scala.kstream._
-import schemas.{Commande, CommandeComplet, DetailsCommande, Facture}
-import serdes.{JSONDeserializerCommandes, JSONDeserializerDtlCommandes, JSONSerializer}
+import schemas.{Commande, CommandeComplet, DetailsCommande}
+import serdes.{JSONDeserializerCmdComplet, JSONDeserializerCommandes, JSONDeserializerDtlCommandes, JSONSerializer}
 import org.apache.kafka.common.serialization.{Serde, Serdes}
-import org.apache.kafka.streams.kstream.JoinWindows
+import org.apache.kafka.streams.kstream.{JoinWindows, Printed}
+
 
 import java.time.Duration
 import java.util.Properties
@@ -17,33 +17,36 @@ object KstreamKstreamJoin extends App {
 
   import org.apache.kafka.streams.scala.ImplicitConversions._
   import org.apache.kafka.streams.scala.Serdes.String
-  import org.apache.kafka.streams.scala.Serdes.Long
 
   implicit val jsonSerdesCommandes : Serde[Commande] = Serdes.serdeFrom(new JSONSerializer[Commande], new JSONDeserializerCommandes)
   implicit val jsonSerdesDetailsCommandes : Serde[DetailsCommande] = Serdes.serdeFrom(new JSONSerializer[DetailsCommande], new JSONDeserializerDtlCommandes)
+  implicit val jsonSerdesCommandesComplet : Serde[CommandeComplet] = Serdes.serdeFrom(new JSONSerializer[CommandeComplet], new JSONDeserializerCmdComplet)
 
-  implicit val consumedCommandes : Consumed[Long, Commande] = Consumed.`with`(new CommandeTimeStampExtractor)(Long, jsonSerdesCommandes)
-  implicit val consumedDetailsCommandes: Consumed[Long, DetailsCommande] = Consumed.`with`(new DtlsCmdTimeStampExtractor)(Long, jsonSerdesDetailsCommandes)
+  implicit val consumedCommandes : Consumed[String, Commande] = Consumed.`with`(new CommandeTimeStampExtractor)(String, jsonSerdesCommandes)
+  implicit val consumedDetailsCommandes: Consumed[String, DetailsCommande] = Consumed.`with`(new DtlsCmdTimeStampExtractor)(String, jsonSerdesDetailsCommandes)
 
-  implicit val produced : Produced[Long, CommandeComplet] = Produced.`with`(Long, String)
+  implicit val produced : Produced[String, CommandeComplet] = Produced.`with`(String, jsonSerdesCommandesComplet)
 
   val props : Properties = new Properties()
-  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kstream-kstream-join")
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kstream-kstream-join10")
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-  props.put("message.timestamp.type", "LogAppendTime")
 
   val str : StreamsBuilder = new StreamsBuilder
-  val kstrCommande : KStream[Long, Commande]  = str.stream[Long, Commande]("commande")(consumedCommandes)
-  val kstrDtlCommande : KStream[Long, DetailsCommande]  = str.stream[Long, DetailsCommande]("commande")(consumedDetailsCommandes)
+  val kstrCommande : KStream[String, Commande]  = str.stream[String, Commande]("commande")(consumedCommandes)
+  val kstrDtlCommande : KStream[String, DetailsCommande]  = str.stream[String, DetailsCommande]("DetailsCommande")(consumedDetailsCommandes)
 
-  val kjoin = kstrDtlCommande.join(kstrCommande)((d : DetailsCommande, c : Commande) =>
-  {
-     CommandeComplet(d.orderid, d.productid, d.shipdate, d.billdate, d.unitprice, d.numunits, d.totalprice, c.city, c.state)
-  },
+  val kstrDtlCommandeGood : KStream[String, DetailsCommande] = kstrDtlCommande.selectKey((k, v) => v.orderid)  // changement de clé
+
+  val kjoin = kstrDtlCommandeGood.join(kstrCommande)((d : DetailsCommande, c : Commande) =>
+    {
+      CommandeComplet(d.orderid, d.productid, d.shipdate, d.billdate, d.unitprice, d.numunits, d.totalprice, c.city, c.state)
+    },
     JoinWindows.of(Duration.ofMinutes(5))
   )
 
-  kjoin.to("commandeComplet")(produced)
+ // kjoin.to("commandeComplet")(produced)
+
+  kjoin.print(Printed.toSysOut().withLabel("Jointure KStreams-à-KStreams"))
 
 
   val topologie : Topology = str.build()
---
+++ src/main/scala/serdes/JSONDeserializerCmdComplet.scala
@@ -0,0 +1,39 @@
+package serdes
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+
+import java.util
+import com.fasterxml.jackson.annotation.JsonInclude
+import org.apache.kafka.common.serialization.Deserializer
+import schemas.CommandeComplet
+
+import scala.reflect._
+
+class JSONDeserializerCmdComplet   extends Deserializer[CommandeComplet]{
+
+  val objetMapper : ObjectMapper = new ObjectMapper()
+  objetMapper.registerModule(DefaultScalaModule)
+  objetMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+  objetMapper.configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, true)
+  objetMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+  override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
+
+  override def deserialize(topic: String, data: Array[Byte]): CommandeComplet= {
+
+    try {
+
+      // processus de désérialisation ici
+      val d = objetMapper.readValue(data, classOf[CommandeComplet])
+      d
+
+    } catch {
+      case e: Exception => throw new Exception(s"Erreur dans la désérialisation du message. Détails de l'erreur : ${e}")
+    }
+
+  }
+
+  override def close(): Unit = {}
+
+}
+
